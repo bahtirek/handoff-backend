6ccad4788425fa2d6f4c181dba2344b1f79c6cd3
Bahti rek <bahtirek@gmail.com>
2026-08-09 22:49:12 -0400

    feat: update Redis configuration and add GitHub Actions workflow for backend deployment

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,6 +1,9 @@
 import Redis from "ioredis";
 import { env } from "../config/env";
 
-
 export const redis =
-    new Redis(env.REDIS_URL);
+  env.REDIS_URL.startsWith("/")
+    ? new Redis({
+        path: env.REDIS_URL
+      })
+    : new Redis(env.REDIS_URL);
